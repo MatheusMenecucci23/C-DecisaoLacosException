7efc47d3ef0de76d977e8976778db180e3b1ee4f
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2021-09-15 14:36:09 -0300

    Adicionar arquivos de projeto.

---
+++ 01/depois/01.ByteBank/ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01.ByteBank
+{
+    class ContaBancaria
+    {
+        private decimal saldo = 0m;
+        public ContaBancaria(decimal saldo)
+        {
+            this.saldo = saldo;
+            Console.WriteLine(this);
+        }
+
+        public void SacarDinheiro(decimal quantia)
+        {
+            //decisão IF
+
+            if (saldo < quantia)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return;
+            }
+            Sacar(quantia);
+            ImprimirComprovante();
+
+
+            ///<image url="$(ProjectDir)\img3.png"/>
+        }
+
+        private bool TemSaldoSuficiente(decimal quantia)
+        {
+            return quantia <= saldo;
+        }
+
+        public void ImprimirComprovante()
+        {
+            Console.WriteLine("Comprovante impresso.");
+        }
+
+        public void Sacar(decimal quantia)
+        {
+            Console.WriteLine($"Sacando {quantia:C}");
+            saldo = saldo - quantia;
+            Console.WriteLine(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Saldo: {saldo:C}";
+        }
+    }
+}
